65564eb3fa804af8e36e8aca7f9c082d34b3164d
bykbyk0401 <bykbyk0401@gmail.com>
2023-11-28 03:30:11 +0900

    feat: Hello Controller 코드를 롬복으로 전환

--- src/test/java/com/gdsc/springstudy/HelloControllerTest.java
+++ src/test/java/com/gdsc/springstudy/HelloControllerTest.java
@@ -8,8 +8,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 //import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 // @RunWith(SpringRunner.class) 제거 (JUnit 5에서는 필요 없음)
@@ -27,5 +30,19 @@ public class HelloControllerTest {
         mvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception{
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(
+                get("/hello/dto")
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
     }
 }
